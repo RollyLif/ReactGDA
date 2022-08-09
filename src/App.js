1d221a89c558afe3d6eb99228568d7e119c9323b
RollyLif <relifungula@gmail.com>
2022-08-09 16:55:35 +0200

    features :basic structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App(){
-  return <h1>Hello</h1>
+  return (
+    <div>
+      <img src="logo.svg" />
+      <h1>React en marche...</h1>
+      <lu>
+        <li>Rolly</li>
+        <li>Kadima</li>
+        <li>Lifungula</li>
+      </lu>
+    </div>
+  )
 }
 
 export default App;
