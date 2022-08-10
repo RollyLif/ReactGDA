44ab4346d7aa25289c252bda611d73b49ecefbc2
RollyLif <relifungula@gmail.com>
2022-08-10 10:40:12 +0200

    features: using function components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-function App(){
-  return (
+function Header(){
+  return(
+    <header>
+    <img src="logo.svg" alt="mon logo" width = "40px"/>
+    </header>
+  );
+}
+
+function Main(){
+  return(
     <div>
-      <img src="logo.svg" alt="mon logo"/>
       <h1>React en marche...</h1>
       <ul>
         <li>Rolly</li>
         <li>Kadima</li>
         <li>Lifungula</li>
       </ul>
+    </div>
+  );
+}
+
+function App(){
+  return (
+    <div>
+      <Header />
+      <Main />
+      
     </div>
   )
 }
