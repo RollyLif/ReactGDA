a3b80dd1f3bd6bda3fac1244ebd986e9423418b0
RollyLif <relifungula@gmail.com>
2022-08-10 11:30:51 +0200

    style: adding style from "App.css"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import "./App.css"
+
 function Header(){
   return(
     <header>
     <img src="logo.svg" alt="mon logo" width = "40px"/>
-    <ul>
+    <ul className="Nav">
       <li>Prix</li>
       <li>A propos</li>
       <li>Contact</li>
